9426b974958ff52fba0e05f701d8e47b9d75b6ea
Krystal <978242273@qq.com>
2023-09-05 23:33:15 +0800

    20230905 docker-demo

---
+++ docker_boot/src/main/java/com/krystal/docker/DockerBootApplication.java
@@ -0,0 +1,15 @@
+package com.krystal.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author krystal
+ * @create 2023-09-05 21:51
+ */
+@SpringBootApplication
+public class DockerBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DockerBootApplication.class, args);
+    }
+}
