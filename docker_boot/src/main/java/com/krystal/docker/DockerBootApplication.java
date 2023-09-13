fdf61f8baedc85a5cffc2230b5c42d598864faed
Krystal <978242273@qq.com>
2023-09-13 20:52:42 +0800

    20230905 docker-demo

--- docker_boot/src/main/java/com/krystal/docker/DockerBootApplication.java
+++ docker_boot/src/main/java/com/krystal/docker/DockerBootApplication.java
@@ -1,5 +1,6 @@
 package com.krystal.docker;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2023-09-05 21:51
  */
 @SpringBootApplication
+@MapperScan("com.krystal.docker.mapper")
 public class DockerBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(DockerBootApplication.class, args);
---
+++ docker_boot/src/main/java/com/krystal/docker/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.krystal.docker.mapper;
+
+/**
+ * @author krystal
+ * @create 2023-09-07 19:17
+ */
+
+import com.krystal.docker.entities.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
